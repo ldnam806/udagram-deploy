c3ca4a1e7fe05a0e9687bd318c30cca81dfa14e4
leDoanNam <namthamlam505@gmail.com>
2022-12-14 01:38:17 +0700

    up

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -4,6 +4,6 @@ export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=Nam205806
 export POSTGRES_DB=postgres
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=namld25
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
